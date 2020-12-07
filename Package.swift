03bdc546067f401a5f080ed6c52b69446ba6653c
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-12-07 22:24:53 +0100

    Error in location of xcframework

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "magick",
-            url: "https://github.com/holzschu/Python-aux/releases/download/1.0/magick.xcframework.zip",
+            url: "https://github.com/holzschu/ImageMagick/releases/download/1.0/magick.xcframework.zip",
             checksum: "35d8c6e7ac0cdfb0ed43be528fd065b003c8491453d6da0169f87569d83bc822"
         )
     ]
