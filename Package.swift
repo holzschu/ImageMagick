cc55a432dea2ea08cf16b219cb982300ac37e2e9
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-12-07 22:33:03 +0100

    Mistake in Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Python-aux",
+    name: "ImageMagick",
     products: [
         .library(name: "ImageMagick", targets: ["magick"])
     ],
