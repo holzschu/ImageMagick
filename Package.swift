03b3999c97e8a4cc83fef721d7174769058e1f64
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-12-07 21:00:14 +0100

    Fixed an issue in Pacakge.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Python-aux",
     products: [
-        .library(name: "magick"])
+        .library(name: "ImageMagick", targets: ["magick"])
     ],
     dependencies: [
     ],
@@ -13,7 +13,7 @@ let package = Package(
         .binaryTarget(
             name: "magick",
             url: "https://github.com/holzschu/Python-aux/releases/download/1.0/magick.xcframework.zip",
-            checksum: "df9d3a7c089c4f31fb2b2b2f90e4dd36e398e363355076487c885347d3aae81a"
+            checksum: "35d8c6e7ac0cdfb0ed43be528fd065b003c8491453d6da0169f87569d83bc822"
         )
     ]
 )
