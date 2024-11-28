689e4320bfa7a89e47f88ffd6cd8ac3676f940e0
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2024-11-28 16:58:12 +0100

    Commit before update to 7.1.1-40

--- PerlMagick/quantum/quantum.pm
+++ PerlMagick/quantum/quantum.pm
@@ -1,4 +1,4 @@
-package Image::Magick::Q16HDRI;
+package Image::Magick::Q8;
 
 #  Copyright 1999-2020 ImageMagick Studio LLC, a non-profit organization
 #  dedicated to making software imaging solutions freely available.
@@ -69,14 +69,14 @@ sub AUTOLOAD {
     goto &$AUTOLOAD;
 }
 
-bootstrap Image::Magick::Q16HDRI $VERSION;
+bootstrap Image::Magick::Q8 $VERSION;
 
 # Preloaded methods go here.
 
 sub new
 {
     my $this = shift;
-    my $class = ref($this) || $this || "Image::Magick::Q16HDRI";
+    my $class = ref($this) || $this || "Image::Magick::Q8";
     my $self = [ ];
     bless $self, $class;
     $self->set(@_) if @_;
@@ -86,7 +86,7 @@ sub new
 sub New
 {
     my $this = shift;
-    my $class = ref($this) || $this || "Image::Magick::Q16HDRI";
+    my $class = ref($this) || $this || "Image::Magick::Q8";
     my $self = [ ];
     bless $self, $class;
     $self->set(@_) if @_;
@@ -102,12 +102,12 @@ __END__
 
 =head1 NAME
 
-Image::Magick::Q16HDRI - objected-oriented Perl interface to ImageMagick (Q16HDRI). Use it to create, edit, compose, or convert bitmap images from within a Perl script.
+Image::Magick::Q8 - objected-oriented Perl interface to ImageMagick (Q8). Use it to create, edit, compose, or convert bitmap images from within a Perl script.
 
 =head1 SYNOPSIS
 
-  use Image::Magick::Q16HDRI;
-  $p = new Image::Magick::Q16HDRI;
+  use Image::Magick::Q8;
+  $p = new Image::Magick::Q8;
   $p->Read("imagefile");
   $p->Set(attribute => value, ...)
   ($a, ...) = $p->Get("attribute", ...)
